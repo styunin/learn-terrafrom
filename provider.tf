a40a6e377bf68369600fd5255ea996f6151f0da3
styunin <styunin@gmail.com>
2022-01-05 16:43:12 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,6 @@
-
+provider "google" {
+  project     = "playground-s-11-935db860" 
+  credentials = file("CREDENTIALS_FILE2.json")
+  region      = "europe-west1"
+  zone        = "europe-west1-d"
+}
