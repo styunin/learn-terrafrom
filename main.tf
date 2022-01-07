b393948108b5f0afaf70c6a3f64c84f07cc55eb9
styunin <styunin@gmail.com>
2022-01-07 16:18:00 +0100

    Update main.tf
    minor editing

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 # Create VPC
 
 resource "google_compute_network" "vpc_network" {
-  project                 = "<your project-id>"
+  project                 = "<your_project>"
   name                    = "ttt-vpc"
   auto_create_subnetworks = true
 }
 
 # Create storage bucket
 resource "google_storage_bucket" "ttt-bucket" {
-  name          = "ttt-bucket-07012022"
-  location      = "EUROPE-WEST4"
-
+  name     = "ttt-bucket-07012022"
+  location = "EUROPE-WEST4"
+}
 
 # Create vm instancce
 
 resource "google_compute_instance" "default" {
   name         = "ttt-vm"
   machine_type = "f1-micro"
-  zone         = "europe-west1-d"
+  zone         = "europe-west4-c"
 
   boot_disk {
     initialize_params {
@@ -36,5 +36,5 @@ resource "google_compute_instance" "default" {
 # Get the IP of the vm
 
 output "ip" {
- value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
