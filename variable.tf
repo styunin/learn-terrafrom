a9157053c99cc16e2bda8e2e6c085bbcf43b5496
styunin <styunin@gmail.com>
2022-01-07 16:49:14 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,13 @@
-# use dufferent variables
-variable region {
+# Use dufferent variables
+
+variable "region" {
   default = "europe-west1"
+}
+
+variable "project" {
+  default = "playground-s-11-b8ed5eb4"
+}
+
+variable "zone" {
+  default = "europe-west1-c"
 }
