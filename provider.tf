aaa6aad16148c98db7b9a4c2553f9a6a55a945e7
styunin <styunin@gmail.com>
2022-01-07 16:50:55 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 #Configure GCP provider
 
 provider "google" {
-  project     = "playground-s-11-b8ed5eb4"
+  project     = var.project
   credentials = file("key.json")
   region      = var.region # see variable.tf
   zone        = var.zone   # see variable.tf
